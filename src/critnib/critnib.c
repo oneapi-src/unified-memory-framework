00b70474f890793d5a0104f1a4346a57f909fb47
Lukasz Dorau <lukasz.dorau@intel.com>
2023-11-14 15:31:55 +0100

    Fix licence of critnib
    Signed-off-by: Lukasz Dorau <lukasz.dorau@intel.com>

--- src/critnib/critnib.c
+++ src/critnib/critnib.c
@@ -2,8 +2,7 @@
  *
  * Copyright (C) 2023 Intel Corporation
  *
- * Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
- * See LICENSE.TXT
+ * Under the Apache License v2.0 with LLVM Exceptions. See LICENSE.TXT.
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  *
  */
