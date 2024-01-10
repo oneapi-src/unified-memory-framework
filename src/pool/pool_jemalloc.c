oneapi-src/unified-memory-framework

commit 459a492de41a647d6dab2364d2f15c9603bf7a71

Fix return values from jemalloc hooks
For OS provider, merge, split and commits are nops.
This means we should return false, not true from
those hooks.

Ref: https://github.com/memkind/memkind/blob/c425635feb0367c43da8fa0deadb50a4bc127988/src/memkind_arena.c

src/pool/pool_jemalloc.c
@@ -136,7 +136,8 @@ static bool arena_extent_commit(extent_hooks_t *extent_hooks, void *addr,
     (void)length;       // unused
     (void)arena_ind;    // unused
 
-    return true; // true means failure (unsupported)
+    // TODO: add this function to the provider API to support Windows and USM
+    return false; // false means success (commit is a nop)
 }
 
 // arena_extent_decommit - an extent decommit function conforms to the extent_decommit_t type
@@ -156,6 +157,7 @@ static bool arena_extent_decommit(extent_hooks_t *extent_hooks, void *addr,
     (void)length;       // unused
     (void)arena_ind;    // unused
 
+    // TODO: add this function to the provider API to support Windows and USM
     return true; // true means failure (unsupported)
 }
 
@@ -215,7 +217,8 @@ static bool arena_extent_split(extent_hooks_t *extent_hooks, void *addr,
     (void)committed;    // unused
     (void)arena_ind;    // unused
 
-    return true; // true means failure (unsupported)
+    // TODO: add this function to the provider API to support Windows and USM
+    return false; // false means success (split is a nop)
 }
 
 // arena_extent_merge - an extent merge function conforms to the extent_merge_t type and optionally
@@ -235,7 +238,8 @@ static bool arena_extent_merge(extent_hooks_t *extent_hooks, void *addr_a,
     (void)committed;    // unused
     (void)arena_ind;    // unused
 
-    return true; // true means failure (unsupported)
+    // TODO: add this function to the provider API to support Windows and USM
+    return false; // false means success (merge is a nop)
 }
 
 // The extent_hooks_t structure comprises function pointers which are described individually below.

commit 4f599ecfa8b3eb417bdc1ed18ba2e331c36fbab2

Fix arena_extent_split() and arena_extent_merge()
Fix a regression introduced by
commit 459a492de41a647d6dab2364d2f15c9603bf7a71
With this commit the jemalloc_pool test fails very often.
This patch fixes this issue.

Ref: 459a492de41a647d6dab2364d2f15c9603bf7a71

Signed-off-by: Lukasz Dorau <lukasz.dorau@intel.com>

src/pool/pool_jemalloc.c
@@ -218,7 +218,7 @@ static bool arena_extent_split(extent_hooks_t *extent_hooks, void *addr,
     (void)arena_ind;    // unused
 
     // TODO: add this function to the provider API to support Windows and USM
-    return false; // false means success (split is a nop)
+    return true; // true means failure (unsupported)
 }
 
 // arena_extent_merge - an extent merge function conforms to the extent_merge_t type and optionally
@@ -239,7 +239,7 @@ static bool arena_extent_merge(extent_hooks_t *extent_hooks, void *addr_a,
     (void)arena_ind;    // unused
 
     // TODO: add this function to the provider API to support Windows and USM
-    return false; // false means success (merge is a nop)
+    return true; // true means failure (unsupported)
 }
 
 // The extent_hooks_t structure comprises function pointers which are described individually below.