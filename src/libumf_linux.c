7577355ebf4581514eda75ebf07b01edcda2e8a0
Krzysztof Swiecicki <krzysztof.swiecicki@intel.com>
2024-02-08 12:06:34 +0100

    Add predefined HOST ALL memspace
    A predefined memspace backed by all NUMA nodes discovered by
    HWLOC. It can be obtained using umfMemspaceHostAllGet() function.

--- src/libumf_linux.c
+++ src/libumf_linux.c
@@ -10,6 +10,7 @@
 #include <stddef.h>
 
 #include "base_alloc_global.h"
+#include "memspace_host_all_internal.h"
 #include "provider_tracking.h"
 
 umf_memory_tracker_handle_t TRACKER = NULL;
@@ -23,6 +24,10 @@ void __attribute__((destructor)) umfDestroy(void) {
     // make sure TRACKER is not used after being destroyed
     TRACKER = NULL;
     umfMemoryTrackerDestroy(t);
+
+#if defined(UMF_BUILD_OS_MEMORY_PROVIDER)
+    umfMemspaceHostAllDestroy();
+#endif
 }
 
 void libumfInit(void) {
---
+++ src/memspaces/memspace_host_all_internal.h
@@ -0,0 +1,26 @@
+/*
+ *
+ * Copyright (C) 2024 Intel Corporation
+ *
+ * Under the Apache License v2.0 with LLVM Exceptions. See LICENSE.TXT.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ *
+ */
+
+#ifndef UMF_MEMSPACE_HOST_ALL_INTERNAL_H
+#define UMF_MEMSPACE_HOST_ALL_INTERNAL_H 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+///
+/// \brief Destroys the predefined host all memspace.
+///
+void umfMemspaceHostAllDestroy(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UMF_MEMSPACE_HOST_ALL_INTERNAL_H */
