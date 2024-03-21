b8e8bab3be1e145e17d05e77987ced6981bdcd88
Lukasz Dorau <lukasz.dorau@intel.com>
2024-03-21 12:28:37 +0100

    Revert "Skip temporarily the assert in umf_ba_linear_destroy()"
    This reverts commit 66ac900d7d08832a57353a7cb650d6bfaa0b9068.
    
    Signed-off-by: Lukasz Dorau <lukasz.dorau@intel.com>

--- src/base_alloc/base_alloc_linear.c
+++ src/base_alloc/base_alloc_linear.c
@@ -251,14 +251,7 @@ void umf_ba_linear_destroy(umf_ba_linear_pool_t *pool) {
     if (pool->metadata.global_n_allocs) {
         fprintf(stderr, "umf_ba_linear_destroy(): global_n_allocs = %zu\n",
                 pool->metadata.global_n_allocs);
-        // This assert fails sporadically on Windows only.
-        // TODO: fix this issue and uncomment this assert
-        // It is safe to comment it out temporarily,
-        // because the linear base allocator is used
-        // in the proxy library only but umf_ba_linear_destroy()
-        // is skipped, so this code is never run in real life.
-        //
-        // assert(pool->metadata.global_n_allocs == 0);
+        assert(pool->metadata.global_n_allocs == 0);
     }
 #endif /* NDEBUG */
 
