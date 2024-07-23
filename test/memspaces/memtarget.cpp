6395af532d3f5582a4c0d238512a955b15ee709c
Łukasz Plewa <lukasz.plewa@intel.com>
2024-07-23 17:56:43 +0200

    add test file and docs for memtarget

---
+++ test/memspaces/memtarget.cpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2024 Intel Corporation
+// Under the Apache License v2.0 with LLVM Exceptions. See LICENSE.TXT.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "memspace_helpers.hpp"
+#include <umf/memspace.h>
+#include <umf/memtarget.h>
+
+using umf_test::test;
