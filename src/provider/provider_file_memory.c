883a548cf7114785082ad6934ee081f7b19f81c0
Lukasz Dorau <lukasz.dorau@intel.com>
2024-09-19 10:58:57 +0200

    Remove unneeded headers
    Remove unneeded headers:
    - provider_devdax_memory_internal.h
    - provider_file_memory_internal.h
    
    Signed-off-by: Lukasz Dorau <lukasz.dorau@intel.com>

--- src/provider/provider_file_memory.c
+++ src/provider/provider_file_memory.c
@@ -15,7 +15,6 @@
 
 #include "base_alloc_global.h"
 #include "critnib.h"
-#include "provider_file_memory_internal.h"
 #include "provider_os_memory_internal.h"
 #include "utils_common.h"
 #include "utils_concurrency.h"
@@ -26,6 +25,32 @@
 #include <umf/providers/provider_file_memory.h>
 
 #define TLS_MSG_BUF_LEN 1024
+
+typedef struct file_memory_provider_t {
+    os_mutex_t lock; // lock for file parameters (size and offsets)
+
+    char path[PATH_MAX]; // a path to the file
+    int fd;              // file descriptor for memory mapping
+    size_t size_fd;      // size of the file used for memory mappings
+    size_t offset_fd;    // offset in the file used for memory mappings
+
+    void *base_mmap;    // base address of the current memory mapping
+    size_t size_mmap;   // size of the current memory mapping
+    size_t offset_mmap; // data offset in the current memory mapping
+
+    unsigned protection; // combination of OS-specific protection flags
+    unsigned visibility; // memory visibility mode
+    size_t page_size;    // minimum page size
+
+    critnib *mmaps; // a critnib map storing mmap mappings (addr, size)
+
+    // A critnib map storing (ptr, fd_offset + 1) pairs. We add 1 to fd_offset
+    // in order to be able to store fd_offset equal 0, because
+    // critnib_get() returns value or NULL, so a value cannot equal 0.
+    // It is needed mainly in the get_ipc_handle and open_ipc_handle hooks
+    // to mmap a specific part of a file.
+    critnib *fd_offset_map;
+} file_memory_provider_t;
 
 typedef struct file_last_native_error_t {
     int32_t native_error;
