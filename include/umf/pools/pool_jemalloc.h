cd475ef11161b8cfdaf7b8c997320bc59b0bb2fa
Lukasz Dorau <lukasz.dorau@intel.com>
2024-12-06 08:09:32 +0100

    Remove the disable_provider_free parameter of jemalloc pool
    Remove the disable_provider_free parameter of jemalloc pool
    and all umfJemallocPoolParams*() API.
    
    Fixes: #904
    
    Signed-off-by: Lukasz Dorau <lukasz.dorau@intel.com>

--- include/umf/pools/pool_jemalloc.h
+++ include/umf/pools/pool_jemalloc.h
@@ -14,34 +14,7 @@
 extern "C" {
 #endif
 
-#include <stdbool.h>
 #include <umf/memory_pool_ops.h>
-
-struct umf_jemalloc_pool_params_t;
-
-/// @brief handle to the parameters of the jemalloc pool.
-typedef struct umf_jemalloc_pool_params_t *umf_jemalloc_pool_params_handle_t;
-
-/// @brief  Create a struct to store parameters of jemalloc pool.
-/// @param  hParams [out] handle to the newly created parameters struct.
-/// @return UMF_RESULT_SUCCESS on success or appropriate error code on failure.
-umf_result_t
-umfJemallocPoolParamsCreate(umf_jemalloc_pool_params_handle_t *hParams);
-
-/// @brief  Destroy parameters struct.
-/// @param  hParams handle to the parameters of the jemalloc pool.
-/// @return UMF_RESULT_SUCCESS on success or appropriate error code on failure.
-umf_result_t
-umfJemallocPoolParamsDestroy(umf_jemalloc_pool_params_handle_t hParams);
-
-/// @brief  Set if \p umfMemoryProviderFree() should never be called.
-/// @param  hParams handle to the parameters of the jemalloc pool.
-/// @param  keepAllMemory \p true if the jemalloc pool should not call
-/// \p umfMemoryProviderFree, \p false otherwise.
-/// @return UMF_RESULT_SUCCESS on success or appropriate error code on failure.
-umf_result_t
-umfJemallocPoolParamsSetKeepAllMemory(umf_jemalloc_pool_params_handle_t hParams,
-                                      bool keepAllMemory);
 
 umf_memory_pool_ops_t *umfJemallocPoolOps(void);
 
