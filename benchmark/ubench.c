33a83ca4102e421b8c93ec5aef279e8cf79381fc
Rafal Rudnicki <rafal.rudnicki@intel.com>
2024-12-07 18:46:46 +0100

    make disjoint pool a C structure

--- benchmark/ubench.c
+++ benchmark/ubench.c
@@ -15,14 +15,11 @@
 
 #include <umf/ipc.h>
 #include <umf/memory_pool.h>
+#include <umf/pools/pool_disjoint.h>
 #include <umf/pools/pool_proxy.h>
 #include <umf/pools/pool_scalable.h>
 #include <umf/providers/provider_level_zero.h>
 #include <umf/providers/provider_os_memory.h>
-
-#ifdef UMF_POOL_DISJOINT_ENABLED
-#include <umf/pools/pool_disjoint.h>
-#endif
 
 #ifdef UMF_POOL_JEMALLOC_ENABLED
 #include <umf/pools/pool_jemalloc.h>
@@ -30,8 +27,7 @@
 
 #include "utils_common.h"
 
-#if (defined UMF_BUILD_LIBUMF_POOL_DISJOINT &&                                 \
-     defined UMF_BUILD_LEVEL_ZERO_PROVIDER && defined UMF_BUILD_GPU_TESTS)
+#if (defined UMF_BUILD_LEVEL_ZERO_PROVIDER && defined UMF_BUILD_GPU_TESTS)
 #include "utils_level_zero.h"
 #endif
 
@@ -244,7 +240,6 @@ UBENCH_EX(simple, proxy_pool_with_os_memory_provider) {
     free(array);
 }
 
-#if (defined UMF_POOL_DISJOINT_ENABLED)
 ////////////////// DISJOINT POOL WITH OS MEMORY PROVIDER
 
 UBENCH_EX(simple, disjoint_pool_with_os_memory_provider) {
@@ -327,7 +322,6 @@ UBENCH_EX(simple, disjoint_pool_with_os_memory_provider) {
     umfMemoryProviderDestroy(os_memory_provider);
     free(array);
 }
-#endif /* (defined UMF_POOL_DISJOINT_ENABLED) */
 
 #if (defined UMF_POOL_JEMALLOC_ENABLED)
 ////////////////// JEMALLOC POOL WITH OS MEMORY PROVIDER
@@ -421,8 +415,7 @@ UBENCH_EX(simple, scalable_pool_with_os_memory_provider) {
 }
 #endif /* (defined UMF_POOL_SCALABLE_ENABLED) */
 
-#if (defined UMF_POOL_DISJOINT_ENABLED &&                                      \
-     defined UMF_BUILD_LEVEL_ZERO_PROVIDER && defined UMF_BUILD_GPU_TESTS)
+#if (defined UMF_BUILD_LEVEL_ZERO_PROVIDER && defined UMF_BUILD_GPU_TESTS)
 static void do_ipc_get_put_benchmark(alloc_t *allocs, size_t num_allocs,
                                      size_t repeats,
                                      umf_ipc_handle_t *ipc_handles) {
@@ -630,7 +623,7 @@ err_destroy_params:
 err_destroy_context:
     utils_ze_destroy_context(context);
 }
-#endif /* (defined UMF_POLL_DISJOINT_ENABLED && defined UMF_BUILD_LEVEL_ZERO_PROVIDER && defined UMF_BUILD_GPU_TESTS) */
+#endif /* (defined UMF_BUILD_LEVEL_ZERO_PROVIDER && defined UMF_BUILD_GPU_TESTS) */
 
 // TODO add IPC benchmark for CUDA
 
