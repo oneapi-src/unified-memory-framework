8fe0ebd78b4b176f7d5cfde2663533948b416c14
sys_tr_ghrunner <sys_tr_ghrunner@intel.com>
2024-12-11 15:47:19 +0100

    codespell added to workflow
    run avaliable on two OS
    
    codespell avaliable on two latests OS
    
    codespell workflow fix
    
    codespell in correct workflow

--- .github/scripts/codespell.sh
+++ .github/scripts/codespell.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -ex
 
-# OUTPUT=$1
 
-SCAN_RESULT=`codespell -H --quiet-level=3 --skip="*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp,./.venv" --ignore-words-list="ASSER,Tne,ba,BA" .\CODE_OF_CONDUCT.md`
+SCAN_RESULT=`codespell -H --quiet-level=3 --skip="*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp,./.venv" --ignore-words-list="ASSER,Tne,ba,BA" CODE_OF_CONDUCT.md`
 echo -e "${SCAN_RESULT}" 
-# echo -e "${SCAN_RESULT}" > ${OUTPUT}
-# cat ${OUTPUT}
 
 if [[ ! -z ${SCAN_RESULT} ]]; then
     exit 1
