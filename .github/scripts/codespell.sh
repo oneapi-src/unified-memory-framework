ba5fa797c260edee60ac76a7105e7cc52161994e
sys_tr_ghrunner <sys_tr_ghrunner@intel.com>
2024-12-11 15:47:19 +0100

    codespell modify

--- .github/scripts/codespell.sh
+++ .github/scripts/codespell.sh
@@ -3,7 +3,7 @@ set -ex
 
 # OUTPUT=$1
 
-SCAN_RESULT=`codespell -H --quiet-level=3 --skip="*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp,./.venv" --ignore-words-list="ASSER,Tne,ba,BA" .\CODE_OF_CONDUCT.md`
+SCAN_RESULT=`codespell -H --quiet-level=3 --skip="*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp,./.venv" --ignore-words-list="ASSER,Tne,ba,BA" CODE_OF_CONDUCT.md`
 echo -e "${SCAN_RESULT}" 
 # echo -e "${SCAN_RESULT}" > ${OUTPUT}
 # cat ${OUTPUT}
