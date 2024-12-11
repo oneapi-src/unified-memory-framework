140402ee176fa4ad1f9df367c1da3c0dd0100ffb
rbanka1 <renata.banka@intel.com>
2024-12-11 20:49:54 +0100

    Check license V2.3 false test 2

--- src/base_alloc/base_alloc_global.c
+++ src/base_alloc/base_alloc_global.c
@@ -1,11 +1,4 @@
-/*
- * Copyright (C) 2024 Intel Corporation
- *
- * Under the Apache License v2.0 with LLVM Exceptions. See LICENSE.TXT.
- * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-*/
 
-/* A MT-safe base allocator */
 
 #include <assert.h>
 #include <stdio.h>
