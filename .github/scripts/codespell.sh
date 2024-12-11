f19144563a6132560c4981f8f24c60e80124c81f
sys_tr_ghrunner <sys_tr_ghrunner@intel.com>
2024-12-11 14:37:33 +0100

    modify script

--- .github/scripts/codespell.sh
+++ .github/scripts/codespell.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-echo "Run codespell..."
-codespell --quiet-level=2 --skip "*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp" -i 0 -L "ASSER,Tne,ba,BA" || true
+OUTPUT=$1
+
+set -e
+# codespell --quiet-level=2 --skip "*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp" -i 0 -L "ASSER,Tne,ba,BA"
+
+SCAN_RESULT=$(codespell --quiet-level=2 --skip "*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp" -i 0 -L "ASSER,Tne,ba,BA")
+echo -e "${SCAN_RESULT}" 
+echo -e "${SCAN_RESULT}" > ${OUTPUT}
+cat ${OUTPUT}
+
+if [[ ! -z ${SCAN_RESULT} ]]; then
+    exit 1
+fi
