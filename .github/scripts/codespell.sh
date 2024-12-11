151e79c07bf91019fc3e350dacd08895986d220d
sys_tr_ghrunner <sys_tr_ghrunner@intel.com>
2024-12-11 15:26:49 +0100

    script fix

--- .github/scripts/codespell.sh
+++ .github/scripts/codespell.sh
@@ -4,7 +4,7 @@ set -ex
 OUTPUT=$1
 # codespell --quiet-level=2 --skip "*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp" -i 0 -L "ASSER,Tne,ba,BA"
 
-SCAN_RESULT=`codespell --quiet-level=3 --skip "*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp" -i 0 -L "ASSER,Tne,ba,BA" -H --count`
+SCAN_RESULT=`codespell --quiet-level=3 --skip="*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp,./.venv" -i 0 -L "ASSER,Tne,ba,BA" -H --count`
 echo -e "${SCAN_RESULT}" 
 echo -e "${SCAN_RESULT}" > ${OUTPUT}
 cat ${OUTPUT}
