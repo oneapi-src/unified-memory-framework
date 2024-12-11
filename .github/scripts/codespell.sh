36eddc3423c6a3621a0fb043eead932c966bbe06
sys_tr_ghrunner <sys_tr_ghrunner@intel.com>
2024-12-11 14:58:59 +0100

    debug script

--- .github/scripts/codespell.sh
+++ .github/scripts/codespell.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+set -ex
 
 OUTPUT=$1
-
-set -ex
 # codespell --quiet-level=2 --skip "*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp" -i 0 -L "ASSER,Tne,ba,BA"
 
-SCAN_RESULT=$(codespell --quiet-level=2 --skip "*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp" -i 0 -L "ASSER,Tne,ba,BA")
+SCAN_RESULT=`codespell --quiet-level=2 --skip "*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp" -L "ASSER,Tne,ba,BA"`
 echo -e "${SCAN_RESULT}" 
 echo -e "${SCAN_RESULT}" > ${OUTPUT}
 cat ${OUTPUT}
