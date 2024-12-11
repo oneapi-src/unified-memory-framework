11de7905f598163425a8eb0c6c6aa284c938406d
sys_tr_ghrunner <sys_tr_ghrunner@intel.com>
2024-12-11 16:27:35 +0100

    codespell in correct workflow

--- .github/scripts/codespell.sh
+++ .github/scripts/codespell.sh
@@ -2,7 +2,7 @@
 set -ex
 
 
-SCAN_RESULT=`codespell -H --quiet-level=3 --skip="*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp,./.venv" --ignore-words-list="ASSER,Tne,ba,BA" CODE_OF_CONDUCT.md`
+SCAN_RESULT=`codespell -H --quiet-level=3 --skip="*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp,./.venv" --ignore-words-list="ASSER,Tne,ba,BA"`
 echo -e "${SCAN_RESULT}" 
 
 if [[ ! -z ${SCAN_RESULT} ]]; then
