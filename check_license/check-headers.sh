ef9aef52439846410e52ea6590b297d69ac4992e
rbanka1 <renata.banka@intel.com>
2024-12-12 16:35:14 +0100

    TEST v1.4

--- check_license/check-headers.sh
+++ check_license/check-headers.sh
@@ -84,7 +84,9 @@ else
 	GIT_COMMAND="diff --name-only $MERGE_BASE $CURRENT_COMMIT"
 fi
 
-FILES=$($GIT $GIT_COMMAND | \
+chmod +x "${SOURCE_ROOT}/check_license/file-exceptions.sh"
+
+FILES=$($GIT $GIT_COMMAND | ${SOURCE_ROOT}/check_license/file-exceptions.sh | \
 	grep    -E -e '*\.[chs]$' -e '*\.[ch]pp$' -e '*\.sh$' -e '*\.py$' \
 		   -e 'TEST*' -e 'Makefile*' -e 'CMakeLists.txt$' -e '*\.cmake$' \
 		   -e '*\.link$' -e '*\.map$' -e '*\.Dockerfile$' -e 'LICENSE$' \
