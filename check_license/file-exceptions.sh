01f2a4d6319d6e576fadeb528c47f60e61c748bb
rbanka1 <renata.banka@intel.com>
2024-12-19 12:25:26 +0100

    final version

--- check_license/file-exceptions.sh
+++ check_license/file-exceptions.sh
@@ -4,9 +4,31 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 # You can add an exception file
-grep -v -E -e '\src\uthash' \
+grep -v -E -e '/src/uthash/.*' \
            -e 'benchmark/ubench.h' \
            -e 'include/umf/proxy_lib_new_delete.h' \
            -e 'scripts/docs_config/conf.py' \
            -e 'src/uthash/utlist.h' \
-           -e '\.yml$'
+           -e 'src/uthash/uthash.h' \
+           -e '\.yml$'\
+           -e '\.clang-format$' \
+           -e '\.md$' \
+           -e '\.cmake-format$' \
+           -e 'CODEOWNERS$' \
+           -e 'scripts/assets/images/.*' \
+           -e 'scripts/docs_config/.*' \
+           -e 'scripts/qemu/configs/.*' \
+           -e '\.txt$' \
+           -e 'test/supp/.*' \
+           -e '\.json$' \
+           -e 'LICENSE.TXT' \
+           -e '.github/workflows/.spellcheck-conf.toml' \
+           -e '.gitignore' \
+           -e '.mailmap' \
+           -e '.trivyignore' \
+           -e 'ChangeLog' \
+           -e '\.cmake.in$' \
+           -e '\.patch$'
+
+
+
