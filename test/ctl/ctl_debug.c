346c1af11edad0a59667f76d3e92cbed4c96e002
Krzysztof Filipek <krzysztof.filipek@intel.com>
2025-01-29 12:06:24 +0100

    [CTL] Add CTL functionality (by handle access)
    This commit introduces the control and introspection mechanism
    that can be accessed using pointer to supported pool or provider.

--- test/ctl/ctl_debug.c
+++ test/ctl/ctl_debug.c
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (C) 2024 Intel Corporation
+ * Copyright (C) 2024-2025 Intel Corporation
  *
  * Under the Apache License v2.0 with LLVM Exceptions. See LICENSE.TXT.
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
@@ -25,11 +25,13 @@ struct ctl *get_debug_ctl(void) { return ctl_debug; }
  * CTL_WRITE_HANDLER(alloc_pattern) -- sets the alloc_pattern field in heap
  */
 static int CTL_WRITE_HANDLER(alloc_pattern)(void *ctx,
-                                            enum ctl_query_source source,
+                                            umf_ctl_query_source_t source,
                                             void *arg,
-                                            struct ctl_index_utlist *indexes) {
+                                            umf_ctl_index_utlist_t *indexes,
+                                            const char *extra_name,
+                                            umf_ctl_query_type_t query_type) {
     /* suppress unused-parameter errors */
-    (void)source, (void)indexes, (void)ctx;
+    (void)source, (void)indexes, (void)ctx, (void)extra_name, (void)query_type;
 
     int arg_in = *(int *)arg;
     alloc_pattern = arg_in;
@@ -40,11 +42,13 @@ static int CTL_WRITE_HANDLER(alloc_pattern)(void *ctx,
  * CTL_READ_HANDLER(alloc_pattern) -- returns alloc_pattern heap field
  */
 static int CTL_READ_HANDLER(alloc_pattern)(void *ctx,
-                                           enum ctl_query_source source,
+                                           umf_ctl_query_source_t source,
                                            void *arg,
-                                           struct ctl_index_utlist *indexes) {
+                                           umf_ctl_index_utlist_t *indexes,
+                                           const char *extra_name,
+                                           umf_ctl_query_type_t query_type) {
     /* suppress unused-parameter errors */
-    (void)source, (void)indexes, (void)ctx;
+    (void)source, (void)indexes, (void)ctx, (void)extra_name, (void)query_type;
 
     int *arg_out = arg;
     *arg_out = alloc_pattern;
@@ -52,11 +56,13 @@ static int CTL_READ_HANDLER(alloc_pattern)(void *ctx,
 }
 
 static int CTL_WRITE_HANDLER(enable_logging)(void *ctx,
-                                             enum ctl_query_source source,
+                                             umf_ctl_query_source_t source,
                                              void *arg,
-                                             struct ctl_index_utlist *indexes) {
+                                             umf_ctl_index_utlist_t *indexes,
+                                             const char *extra_name,
+                                             umf_ctl_query_type_t query_type) {
     /* suppress unused-parameter errors */
-    (void)source, (void)indexes, (void)ctx;
+    (void)source, (void)indexes, (void)ctx, (void)extra_name, (void)query_type;
 
     int arg_in = *(int *)arg;
     enable_logging = arg_in;
@@ -64,33 +70,40 @@ static int CTL_WRITE_HANDLER(enable_logging)(void *ctx,
 }
 
 static int CTL_READ_HANDLER(enable_logging)(void *ctx,
-                                            enum ctl_query_source source,
+                                            umf_ctl_query_source_t source,
                                             void *arg,
-                                            struct ctl_index_utlist *indexes) {
+                                            umf_ctl_index_utlist_t *indexes,
+                                            const char *extra_name,
+                                            umf_ctl_query_type_t query_type) {
     /* suppress unused-parameter errors */
-    (void)source, (void)indexes, (void)ctx;
+    (void)source, (void)indexes, (void)ctx, (void)extra_name, (void)query_type;
 
     int *arg_out = arg;
     *arg_out = enable_logging;
     return 0;
 }
 
-static int CTL_WRITE_HANDLER(log_level)(void *ctx, enum ctl_query_source source,
+static int CTL_WRITE_HANDLER(log_level)(void *ctx,
+                                        umf_ctl_query_source_t source,
                                         void *arg,
-                                        struct ctl_index_utlist *indexes) {
+                                        umf_ctl_index_utlist_t *indexes,
+                                        const char *extra_name,
+                                        umf_ctl_query_type_t query_type) {
     /* suppress unused-parameter errors */
-    (void)source, (void)indexes, (void)ctx;
+    (void)source, (void)indexes, (void)ctx, (void)extra_name, (void)query_type;
 
     int arg_in = *(int *)arg;
     log_level = arg_in;
     return 0;
 }
 
-static int CTL_READ_HANDLER(log_level)(void *ctx, enum ctl_query_source source,
+static int CTL_READ_HANDLER(log_level)(void *ctx, umf_ctl_query_source_t source,
                                        void *arg,
-                                       struct ctl_index_utlist *indexes) {
+                                       umf_ctl_index_utlist_t *indexes,
+                                       const char *extra_name,
+                                       umf_ctl_query_type_t query_type) {
     /* suppress unused-parameter errors */
-    (void)source, (void)indexes, (void)ctx;
+    (void)source, (void)indexes, (void)ctx, (void)extra_name, (void)query_type;
 
     int *arg_out = arg;
     *arg_out = log_level;
@@ -103,15 +116,15 @@ static const struct ctl_argument CTL_ARG(enable_logging) = CTL_ARG_BOOLEAN;
 
 static const struct ctl_argument CTL_ARG(log_level) = CTL_ARG_INT;
 
-static const struct ctl_node CTL_NODE(heap)[] = {CTL_LEAF_RW(alloc_pattern),
-                                                 CTL_LEAF_RW(enable_logging),
-                                                 CTL_LEAF_RW(log_level),
+static const umf_ctl_node_t CTL_NODE(heap)[] = {CTL_LEAF_RW(alloc_pattern),
+                                                CTL_LEAF_RW(enable_logging),
+                                                CTL_LEAF_RW(log_level),
+
+                                                CTL_NODE_END};
+
+static const umf_ctl_node_t CTL_NODE(debug)[] = {CTL_CHILD(heap),
 
                                                  CTL_NODE_END};
-
-static const struct ctl_node CTL_NODE(debug)[] = {CTL_CHILD(heap),
-
-                                                  CTL_NODE_END};
 
 /*
  * debug_ctl_register -- registers ctl nodes for "debug" module
