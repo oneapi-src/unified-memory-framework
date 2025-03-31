ba470d1d9eab7ad35b056edb981f31d714249e4d
Patryk Kaminski <patryk.kaminski@intel.com>
2025-03-31 12:00:26 +0200

    Remove UMF_DISABLE_HWLOC option

--- src/memspaces/memspace_highest_bandwidth.c
+++ src/memspaces/memspace_highest_bandwidth.c
@@ -14,16 +14,15 @@
 #include <umf.h>
 #include <umf/experimental/memspace.h>
 
-// UMF_MEMSPACE_HIGHEST_BANDWIDTH requires HWLOC
-// Additionally, it is currently unsupported on Win
-#if defined(_WIN32) || defined(UMF_NO_HWLOC)
+// UMF_MEMSPACE_HIGHEST_BANDWIDTH is currently unsupported on Win
+#if defined(_WIN32)
 
 umf_const_memspace_handle_t umfMemspaceHighestBandwidthGet(void) {
     // not supported
     return NULL;
 }
 
-#else // !defined(_WIN32) && !defined(UMF_NO_HWLOC)
+#else // !defined(_WIN32)
 
 #include "base_alloc_global.h"
 #include "memspace_internal.h"
@@ -119,4 +118,4 @@ umf_const_memspace_handle_t umfMemspaceHighestBandwidthGet(void) {
     return UMF_MEMSPACE_HIGHEST_BANDWIDTH;
 }
 
-#endif // !defined(_WIN32) && !defined(UMF_NO_HWLOC)
+#endif // !defined(_WIN32)
