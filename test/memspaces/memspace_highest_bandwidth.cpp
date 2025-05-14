19d5a217149ef411cbff957394d174d64a5abd0b
Łukasz Plewa <lukasz.plewa@intel.com>
2025-05-14 16:33:52 +0200

    test correct memory bind with highest bandwidth memspace
    ref: #1289

--- test/memspaces/memspace_highest_bandwidth.cpp
+++ test/memspaces/memspace_highest_bandwidth.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2024 Intel Corporation
+// Copyright (C) 2024-2025 Intel Corporation
 // Under the Apache License v2.0 with LLVM Exceptions. See LICENSE.TXT.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
@@ -7,6 +7,7 @@
 #include "memspace_fixtures.hpp"
 #include "memspace_helpers.hpp"
 #include "memspace_internal.h"
+#include "numa_helpers.hpp"
 #include "test_helpers.h"
 
 static void canQueryBandwidth(size_t nodeId) {
@@ -49,3 +50,80 @@ INSTANTIATE_TEST_SUITE_P(memspaceLowestLatencyProviderTest,
                          ::testing::Values(memspaceGetParams{
                              canQueryBandwidth,
                              umfMemspaceHighestBandwidthGet}));
+
+TEST_F(numaNodesTest, PerCoreBandwidthPlacement) {
+    const size_t allocSize = 4096;
+    unsigned int numCores = std::thread::hardware_concurrency();
+    if (numCores == 0) {
+        numCores = 1;
+    }
+
+    canQueryBandwidth(0);
+    if (IS_SKIPPED_OR_FAILED()) {
+        GTEST_SKIP() << "Error: hwloc_memattr_get_value returned 0";
+    }
+
+    std::vector<std::thread> workers;
+    workers.reserve(numCores);
+
+    for (unsigned int i = 0; i < numCores; ++i) {
+        workers.emplace_back([cpuIndex = i]() {
+            hwloc_topology_t topo = nullptr;
+            ASSERT_EQ(hwloc_topology_init(&topo), 0);
+            ASSERT_EQ(hwloc_topology_load(topo), 0);
+
+            hwloc_bitmap_t target = hwloc_bitmap_alloc();
+            hwloc_bitmap_only(target, cpuIndex);
+            ASSERT_EQ(
+                hwloc_set_cpubind(topo, target,
+                                  HWLOC_CPUBIND_THREAD | HWLOC_CPUBIND_STRICT),
+                0);
+            hwloc_bitmap_free(target);
+
+            hwloc_location initiator;
+            hwloc_bitmap_t here = hwloc_bitmap_alloc();
+            ASSERT_EQ(
+                hwloc_get_cpubind(topo, here,
+                                  HWLOC_CPUBIND_THREAD | HWLOC_CPUBIND_STRICT),
+                0);
+            initiator.location.cpuset = here;
+            initiator.type = HWLOC_LOCATION_TYPE_CPUSET;
+
+            hwloc_obj_t bestnode;
+            hwloc_memattr_get_best_target(topo, HWLOC_MEMATTR_ID_BANDWIDTH,
+                                          &initiator, 0, &bestnode, nullptr);
+
+            void *ptr_hwloc = hwloc_alloc_membind(
+                topo, allocSize, bestnode->nodeset, HWLOC_MEMBIND_BIND,
+                HWLOC_MEMBIND_BYNODESET);
+            ASSERT_NE(ptr_hwloc, nullptr);
+            memset(ptr_hwloc, 0, allocSize);
+
+            auto memspace = umfMemspaceHighestBandwidthGet();
+            ASSERT_NE(memspace, nullptr);
+
+            umf_memory_provider_handle_t provider;
+            ASSERT_EQ(umfMemoryProviderCreateFromMemspace(memspace, nullptr,
+                                                          &provider),
+                      UMF_RESULT_SUCCESS);
+
+            void *ptr_umf = nullptr;
+            ASSERT_EQ(umfMemoryProviderAlloc(provider, allocSize, 0, &ptr_umf),
+                      UMF_RESULT_SUCCESS);
+            ASSERT_NE(ptr_umf, nullptr);
+            memset(ptr_umf, 0, allocSize);
+
+            ASSERT_NODE_EQ(ptr_umf, ptr_hwloc);
+
+            umfMemoryProviderFree(provider, ptr_umf, allocSize);
+            umfMemoryProviderDestroy(provider);
+            hwloc_free(topo, ptr_hwloc, allocSize);
+            hwloc_bitmap_free(here);
+            hwloc_topology_destroy(topo);
+        });
+    }
+
+    for (auto &t : workers) {
+        t.join();
+    }
+}
