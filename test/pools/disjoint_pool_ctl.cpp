2c7329a6bd849490327a870e2ff056165738e2f1
Krzysztof Filipek <krzysztof.filipek@intel.com>
2025-05-28 12:03:32 +0200

    [CTL] Add allocation counter for disjoint pool

--- test/pools/disjoint_pool_ctl.cpp
+++ test/pools/disjoint_pool_ctl.cpp
@@ -2,6 +2,7 @@
 // Under the Apache License v2.0 with LLVM Exceptions. See LICENSE.TXT.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exceptiongi
 
+#include <cstdint>
 #include <gtest/gtest.h>
 #include <umf/memory_pool.h>
 #include <umf/memory_provider.h>
@@ -84,6 +85,77 @@ class ProviderWrapper {
     const umf_memory_provider_ops_t *m_providerOps;
     void *m_params;
 };
+
+TEST_F(test, disjointCtlAllocationBalance) {
+    umf_os_memory_provider_params_handle_t os_memory_provider_params = nullptr;
+    if (UMF_RESULT_ERROR_NOT_SUPPORTED ==
+        umfOsMemoryProviderParamsCreate(&os_memory_provider_params)) {
+        GTEST_SKIP() << "OS memory provider is not supported!";
+    }
+
+    ProviderWrapper providerWrapper(umfOsMemoryProviderOps(),
+                                    os_memory_provider_params);
+    if (providerWrapper.get() == NULL) {
+        GTEST_SKIP() << "OS memory provider is not supported!";
+    }
+
+    umf_disjoint_pool_params_handle_t params = nullptr;
+    ASSERT_SUCCESS(umfDisjointPoolParamsCreate(&params));
+
+    // Set max poolable size to a reasonable value
+    ASSERT_SUCCESS(
+        umfDisjointPoolParamsSetMaxPoolableSize(params, 1024 * 1024));
+
+    // Set the capacity of the pool to a reasonable value
+    ASSERT_SUCCESS(
+        umfDisjointPoolParamsSetSlabMinSize(params, 64 * 1024)); // 64 KiB
+
+    // Set the trace level to 3 to enable allocation balance tracking
+    ASSERT_SUCCESS(umfDisjointPoolParamsSetTrace(params, 3));
+
+    PoolWrapper poolWrapper(providerWrapper.get(), umfDisjointPoolOps(),
+                            params);
+
+    // Check that the allocation balance is zero
+    uint64_t allocation_balance = 0;
+    ASSERT_SUCCESS(umfCtlGet("umf.pool.by_handle.disjoint.allocation_balance",
+                             poolWrapper.get(), &allocation_balance,
+                             sizeof(allocation_balance)));
+    ASSERT_EQ(allocation_balance, 0ull);
+
+    // Allocate some memory from the pool
+    size_t allocation_size = 64; // 64 B
+    const uint64_t max_allocations = 2;
+    void *ptr[max_allocations] = {nullptr};
+    uint64_t i = 0;
+    while (i < max_allocations) {
+        ptr[i] = umfPoolMalloc(poolWrapper.get(), allocation_size);
+        ASSERT_NE(ptr[i], nullptr);
+        ++i;
+    }
+
+    // Check the allocation balance after allocations
+    ASSERT_SUCCESS(umfCtlGet("umf.pool.by_handle.disjoint.allocation_balance",
+                             poolWrapper.get(), &allocation_balance,
+                             sizeof(allocation_balance)));
+    ASSERT_EQ(allocation_balance, max_allocations);
+
+    // Check balance after freeing the allocations
+    for (uint64_t j = 0; j < max_allocations; ++j) {
+        if (ptr[j]) {
+            ASSERT_EQ(umfPoolFree(poolWrapper.get(), ptr[j]),
+                      UMF_RESULT_SUCCESS);
+        }
+    }
+    allocation_balance = 123;
+    ASSERT_SUCCESS(umfCtlGet("umf.pool.by_handle.disjoint.allocation_balance",
+                             poolWrapper.get(), &allocation_balance,
+                             sizeof(allocation_balance)));
+    ASSERT_EQ(allocation_balance, 0ull);
+
+    ASSERT_SUCCESS(umfDisjointPoolParamsDestroy(params));
+    ASSERT_SUCCESS(umfOsMemoryProviderParamsDestroy(os_memory_provider_params));
+}
 
 TEST_F(test, disjointCtlName) {
     umf_os_memory_provider_params_handle_t os_memory_provider_params = nullptr;
